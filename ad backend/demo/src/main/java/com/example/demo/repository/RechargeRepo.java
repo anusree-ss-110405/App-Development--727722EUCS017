b2f53e382d2c4761dcd292edc3272db1573eddc4
anusree-ss-110405 <sreeanu110405@gmail.com>
2024-08-01 10:58:34 +0530

    repo created

--- ad backend/demo/src/main/java/com/example/demo/repository/RechargeRepo.java
+++ ad backend/demo/src/main/java/com/example/demo/repository/RechargeRepo.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RechargeRepo extends JpaRepository{
+import com.example.demo.model.Recharge;
+
+public interface RechargeRepo extends JpaRepository<Recharge,Integer>{
 
 }
--- ad backend/demo/src/main/java/com/example/demo/repository/RegistrationRepo.java
+++ ad backend/demo/src/main/java/com/example/demo/repository/RegistrationRepo.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegistrationRepo extends JpaRepository{
+import com.example.demo.model.Registration;
+
+public interface RegistrationRepo extends JpaRepository<Registration,Integer>{
 
 }
