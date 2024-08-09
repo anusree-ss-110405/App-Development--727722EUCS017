e698f21082c18f8ebe3bec0eef828bf2a73bb497
anusree-ss-110405 <sreeanu110405@gmail.com>
2024-08-09 10:49:19 +0530

    commit

--- ad backend/demo/src/main/java/com/example/demo/Repository/MobilePlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.mobilePlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MobilePlanRepository extends JpaRepository<mobilePlan, Integer> {
-}
--- ad backend/demo/src/main/java/com/example/demo/Repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<transaction, Integer> {
-}
